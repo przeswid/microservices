5208fa05b53d8f45793d259c17c4d698e605d683
Przemek Świda <przemyslaw.swida@boldare.com>
2022-01-09 10:18:10 +0100

    Initial commit

---
+++ cloudconfig/src/main/java/p/s/microservices/serverconfig/ConfigServer.java
@@ -0,0 +1,14 @@
+package p.s.microservices.serverconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServer {
+
+    public static void main(String[] arguments) {
+        SpringApplication.run(ConfigServer.class, arguments);
+    }
+}
